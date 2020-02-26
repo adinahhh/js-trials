f42b298b25dc82cb5a50c23203cfc11357c7204d
Your Name <you@example.com>
2020-02-26 14:46:38 -0800

    up to further study

--- furtherStudy.js
+++ furtherStudy.js
@@ -3,9 +3,21 @@
 
 function wordsInCommon(words1, words2) {
   // Replace this with your code
-}
 
+  setWords1 = new Set(words1);
+  setWords2 = new Set(words2);
+
+  result = new Set();
+
+  for (const word of setWords1) {
+    if (setWords2.has(word)) {
+      result.add(word);
+    }
+    return result.from();
+  }
+}
 
 function kidsGame(names) {
   // Replace this with your code
 }
+
